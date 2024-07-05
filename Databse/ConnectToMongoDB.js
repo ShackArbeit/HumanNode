2f94382352617ba27d19ecfcbe4ea074b79698ac
Shack <g0972222165@gmail.com>
2024-07-05 13:16:12 +0800

    Correct Connect

--- Databse/ConnectToMongoDB.js
+++ Databse/ConnectToMongoDB.js
@@ -1,22 +1,26 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
-let isConnected = false; 
-const url = "mongodb+srv://wang8119:wang8119@cluster0.w3kipgk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+let isConnected = false;
+const url = "mongodb+srv://wang8119:wang8119@cluster0.w3kipgk.mongodb.net/myWebsite?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToDB = async () => {
   mongoose.set('strictQuery', true);
-  if(isConnected) {
+  if (isConnected) {
     console.log('已經連線到 MongoDB 了 !');
     return;
   }
   try {
     await mongoose.connect(url, {
-      dbName: "myWebsite",
-    })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "myWebsite", // 指定資料庫名稱
+    });
     isConnected = true;
-    console.log('你已經成功連線了 !')
+    console.log('你已經成功連線了 !');
   } catch (error) {
-    console.log('連線失敗',error);
+    console.error('連線失敗', error);
+    isConnected = false; // 連線失敗時將 isConnected 設為 false
   }
 }
-module.exports=connectToDB
+
+module.exports = connectToDB;
